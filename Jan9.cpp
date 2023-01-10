2d515783bfc9d0058027826d5ff4109906b95b0c
Arjun Solanki <92087686+arjunsolanki2612@users.noreply.github.com>
2023-01-10 10:55:25 +0530

    Create Jan9.cpp

---
+++ Jan9.cpp
@@ -0,0 +1,32 @@
+144. Binary Tree Preorder Traversal
+Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+   
+       
+    vector<int>ans;
+    vector<int> preorderTraversal(TreeNode* root) {
+        
+        if(root!=NULL){
+        ans.push_back(root->val);
+        preorderTraversal(root->left);
+        preorderTraversal(root->right);
+        }
+    
+        return ans;
+
+
+    }
+};
